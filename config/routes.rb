2089c7c34703b5f82ca1c34153b1209ca2179d88
Heru Kristanto <herukristanto90@gmail.com>
2019-07-15 00:13:31 +0700

    api backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :artists do
+        resources :records
+      end
+    end
+  end
+
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#index"
 end
